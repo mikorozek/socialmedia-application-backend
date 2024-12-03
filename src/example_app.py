d284f5c7f93d9364ae4b635e4a243b63b083edca
Marcin Lisowski <mnlisows@gmail.com>
2024-12-04 00:32:55 +0100

    removed python app

--- src/example_app.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def main():
-    print("Hello, World!")
-    response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-    print(response.json())
-    print("cos")
-if __name__ == '__main__':
-    main()
