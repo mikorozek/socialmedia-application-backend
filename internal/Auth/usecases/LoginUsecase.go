4a5ea196945043c14707591087a432a136007a28
mrozek <01178582@pw.edu.pl>
2024-12-17 22:54:15 +0100

    SCRUM-17 REFACTOR: Changed the user model field name to valid one

--- internal/Auth/usecases/LoginUsecase.go
+++ internal/Auth/usecases/LoginUsecase.go
@@ -3,15 +3,14 @@ package usecases
 
 import (
 	"errors"
-	"socialmedia-app/internal/shared/models"
-	"socialmedia-app/internal/shared/repositories"
+	"socialmedia-backend/internal/shared/models"
+	"socialmedia-backend/internal/shared/repositories"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginUsecase struct {
 	userRepo *repositories.UserRepository
-	// Jira test
 }
 
 func NewLoginUsecase() *LoginUsecase {
@@ -26,7 +25,7 @@ func (u *LoginUsecase) Execute(email, password string) (*models.User, error) {
 		return nil, errors.New("user not found")
 	}
 
-	if err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password)); err != nil {
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password)); err != nil {
 		return nil, errors.New("invalid credentials")
 	}
 
