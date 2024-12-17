18cbfe2deb7907811918aed81b1fd8e140115198
mrozek <01178582@pw.edu.pl>
2024-12-17 21:43:44 +0100

    SCRUM-16 IMPLEMENT: test jira

--- internal/shared/models/user.go
+++ internal/shared/models/user.go
@@ -4,5 +4,5 @@ type User struct {
 	ID				uint		`gorm:"primarykey"`
 	Username		string		`gorm:"unique;not null"`
 	Email			string 		`gorm:"unique;not null"`
-	PasswordHash	string		`gorm:"not null"`
+	PasswordHash	string		`gorm:"not nul"`
 };
