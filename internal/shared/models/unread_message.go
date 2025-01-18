50d8c3d31c5e3c3d259974dd638a907dd68dc017
Marcin Lisowski <mnlisows@gmail.com>
2025-01-18 11:17:21 +0100

    REFACTOR: UnreadMessage replaced with UnreadConversation

--- internal/shared/models/unread_message.go
+++ internal/shared/models/unread_message.go
@@ -1,10 +1,13 @@
 package models
 
-type UnreadMessage struct {
-	MessageID      uint         `gorm:"primarykey"`
-	Message        Message      `gorm:"foreignKey:MessageID"`
-	UserID         uint         `gorm:"primarykey"`
-	User           User         `gorm:"foreignKey:UserID"`
-	ConversationID uint         `gorm:"not null"`
-	Conversation   Conversation `gorm:"foreignKey:ConversationID"`
+import "time"
+
+type UnreadConversation struct {
+	ID                 uint         `gorm:"primarykey"`
+	ConversationID     uint         `gorm:"not null"`
+	Conversation       Conversation `gorm:"foreignKey:ConversationID"`
+	UserID             uint         `gorm:"not null"`
+	User               User         `gorm:"foreignKey:UserID"`
+	LastMessageContent string       `gorm:"not null"`
+	LastMessageTime    time.Time    `gorm:"not null"`
 }
