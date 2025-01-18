fbe8c853442a5dbf1bdc3c8efc0c79bd14716ff0
Marcin Lisowski <mnlisows@gmail.com>
2025-01-18 13:27:21 +0100

    REFACTOR: returning json reformatted

--- internal/shared/models/unread_conversation.go
+++ internal/shared/models/unread_conversation.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type UnreadConversation struct {
-	ID                 uint         `gorm:"primarykey"`
-	ConversationID     uint         `gorm:"not null"`
-	Conversation       Conversation `gorm:"foreignKey:ConversationID"`
-	UserID             uint         `gorm:"not null"`
-	User               User         `gorm:"foreignKey:UserID"`
-	LastMessageContent string       `gorm:"not null"`
-	LastMessageTime    time.Time    `gorm:"not null"`
+	ID                 uint         `gorm:"primarykey" json:"id"`
+	ConversationID     uint         `gorm:"not null" json:"conversation_id"`
+	Conversation       Conversation `gorm:"foreignKey:ConversationID" json:"conversation"`
+	UserID             uint         `gorm:"not null" json:"user_id"`
+	User               User         `gorm:"foreignKey:UserID" json:"user"`
+	LastMessageContent string       `gorm:"not null" json:"last_message_content"`
+	LastMessageTime    time.Time    `gorm:"not null" json:"last_message_time"`
 }
--- internal/shared/models/user.go
+++ internal/shared/models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	ID           uint            `gorm:"primarykey"`
-	Username     string          `gorm:"unique;not null"`
-	Email        string          `gorm:"unique;not null"`
-	PasswordHash string          `gorm:"not null"`
-	Conversation []*Conversation `gorm:"many2many:conversation_user;constraint:OnDelete:CASCADE"`
-	Messages     []Message       `gorm:"constraint:OnDelete:CASCADE"`
+	ID           uint            `gorm:"primarykey" json:"id"`
+	Username     string          `gorm:"unique;not null" json:"username"`
+	Email        string          `gorm:"unique;not null" json:"email"`
+	PasswordHash string          `gorm:"not null" json:"password_hash"`
+	Conversation []*Conversation `gorm:"many2many:conversation_user;constraint:OnDelete:CASCADE" json:"conversation"`
+	Messages     []Message       `gorm:"constraint:OnDelete:CASCADE" json:"messages"`
 }
