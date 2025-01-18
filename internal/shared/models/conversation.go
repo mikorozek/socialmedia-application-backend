0568a863e336d2402e5a2f07cf21b876d0d297dd
Marcin Lisowski <mnlisows@gmail.com>
2025-01-18 11:17:45 +0100

    REFACTOR: Users relation in conversation model

--- internal/shared/models/conversation.go
+++ internal/shared/models/conversation.go
@@ -3,5 +3,5 @@ package models
 type Conversation struct {
 	ID       uint      `gorm:"primarykey"`
 	Messages []Message `gorm:"constraint:OnDelete:CASCADE"`
-	Users    []User    `gorm:"many2many:conversation_users;"`
+	Users    []*User   `gorm:"many2many:conversation_users;"`
 }
