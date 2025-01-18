4db3a231186faa4a28c2d0eecb032ce2456f24d4
Maksymilian Bilski <maksde25@gmail.com>
2025-01-18 02:23:12 +0100

    IMPLEMENT: message model

--- internal/shared/models/message.go
+++ internal/shared/models/message.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type Message struct {
-	ID             uint      `gorm:"primarykey"`
-	ConversationID uint      `gorm:"not null"`
-	UserID         uint      `gorm:"not null"`
-	Content        string    `gorm:"not null"`
-	MessageDate    time.Time `gorm:"not null"`
+	ID             uint         `gorm:"primarykey"`
+	ConversationID uint         `gorm:"not null"`
+	Conversation   Conversation `gorm:"foreignKey:ConversationID"`
+	UserID         uint         `gorm:"not null"`
+	Content        string       `gorm:"not null"`
+	MessageDate    time.Time    `gorm:"not null"`
 	PhotoURL       string
 }
