00b4795890d6bb39bf1b649841f0e3ddffd675ae
Marcin Lisowski <mnlisows@gmail.com>
2025-01-18 11:18:20 +0100

    REFACTOR: Message missing user foreign key

--- internal/shared/models/message.go
+++ internal/shared/models/message.go
@@ -7,6 +7,7 @@ type Message struct {
 	ConversationID uint         `gorm:"not null"`
 	Conversation   Conversation `gorm:"foreignKey:ConversationID"`
 	UserID         uint         `gorm:"not null"`
+	User           User         `gorm:"foreignKey:UserID"`
 	Content        string       `gorm:"not null"`
 	MessageDate    time.Time    `gorm:"not null"`
 	PhotoURL       string
