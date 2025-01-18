70d00e37eba55b18bf1430b0e9706b87065ba63d
Marcin Lisowski <mnlisows@gmail.com>
2025-01-18 02:24:24 +0100

    IMPLEMENT: Conversation model

--- internal/shared/models/conversation.go
+++ internal/shared/models/conversation.go
@@ -1,7 +1,7 @@
 package models
 
 type Conversation struct {
-	ID       uint `gorm:"primarykey"`
-	Messages []Message
-	Users    []User `gorm:"many2many:conversation_users;"`
+	ID       uint      `gorm:"primarykey"`
+	Messages []Message `gorm:"constraint:OnDelete:CASCADE"`
+	Users    []User    `gorm:"many2many:conversation_users;"`
 }
