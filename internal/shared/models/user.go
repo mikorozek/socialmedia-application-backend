df7ba300cc65f4bd5be701d2c91cbf591e1d94a3
Marcin Lisowski <mnlisows@gmail.com>
2025-01-18 11:18:01 +0100

    REFACTOR: User missing relations with conversation and messages

--- internal/shared/models/user.go
+++ internal/shared/models/user.go
@@ -1,8 +1,10 @@
 package models
 
 type User struct {
-	ID				uint		`gorm:"primarykey"`
-	Username		string		`gorm:"unique;not null"`
-	Email			string 		`gorm:"unique;not null"`
-	PasswordHash	string		`gorm:"not null"`
-};
+	ID           uint            `gorm:"primarykey"`
+	Username     string          `gorm:"unique;not null"`
+	Email        string          `gorm:"unique;not null"`
+	PasswordHash string          `gorm:"not null"`
+	Conversation []*Conversation `gorm:"many2many:conversation_user;constraint:OnDelete:CASCADE"`
+	Messages     []Message       `gorm:"constraint:OnDelete:CASCADE"`
+}
