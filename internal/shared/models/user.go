99b582c35f6beddc661623c4e466d5a99caf888b
Marcin Lisowski <mnlisows@gmail.com>
2025-01-21 23:13:47 +0100

    REFACTOR: Description attribute added to User model

--- internal/shared/models/user.go
+++ internal/shared/models/user.go
@@ -5,6 +5,7 @@ type User struct {
 	Username     string          `gorm:"unique;not null" json:"username"`
 	Email        string          `gorm:"unique;not null" json:"email"`
 	PasswordHash string          `gorm:"not null" json:"password_hash"`
+	Description  string          `gorm:"type:text" json:"description"`
 	Conversation []*Conversation `gorm:"many2many:conversation_user;constraint:OnDelete:CASCADE" json:"conversation"`
 	Messages     []Message       `gorm:"constraint:OnDelete:CASCADE" json:"messages"`
 }
