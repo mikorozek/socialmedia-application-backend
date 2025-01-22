36bdaa01224403be2b43a4b06c011fce35eaaff6
Marcin Lisowski <mnlisows@gmail.com>
2025-01-22 03:30:02 +0100

    FIX: Password Hash not returned to json

--- internal/shared/models/user.go
+++ internal/shared/models/user.go
@@ -4,7 +4,7 @@ type User struct {
 	ID           uint            `gorm:"primarykey" json:"id"`
 	Username     string          `gorm:"unique;not null" json:"username"`
 	Email        string          `gorm:"unique;not null" json:"email"`
-	PasswordHash string          `gorm:"not null" json:"password_hash"`
+	PasswordHash string          `gorm:"not null" json:"-"`
 	Description  string          `gorm:"type:text" json:"description"`
 	Conversation []*Conversation `gorm:"many2many:conversation_user;constraint:OnDelete:CASCADE" json:"conversation"`
 	Messages     []Message       `gorm:"constraint:OnDelete:CASCADE" json:"messages"`
